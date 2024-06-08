464713acd57f48d7ac4a3194fd965c73c2e42606
AndyWu <s0939859391@gmail.com>
2024-06-08 11:44:55 +0800

    feat: remove no use page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="text-primary-100">HomeView</p>
   </main>
 </template>
