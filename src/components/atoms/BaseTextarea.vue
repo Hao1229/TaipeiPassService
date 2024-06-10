7e127ca393b826c6d747917c90bda2e0edf20233
AndyWu <s0939859391@gmail.com>
2024-06-10 20:01:22 +0800

    feat: add component BaseTextarea

---
+++ src/components/atoms/BaseTextarea.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const inputValue = defineModel();
+</script>
+
+<template>
+  <textarea v-model="inputValue" maxlength="4000" class="base-textarea" />
+</template>
+
+<style lang="postcss">
+.base-textarea {
+  @apply bg-gray-100 h-32;
+  @apply p-3;
+  @apply rounded-lg outline-none;
+  @apply overflow-y-auto;
+}
+</style>
