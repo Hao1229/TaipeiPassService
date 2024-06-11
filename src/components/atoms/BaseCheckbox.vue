c79dd2f7e48115b1f7ec6228d7bae0b02c14347b
AndyWu <s0939859391@gmail.com>
2024-06-11 20:30:37 +0800

    feat: add component BaseCheckbox

---
+++ src/components/atoms/BaseCheckbox.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import type { Option } from '@/components/atoms/BaseSelect.vue';
+
+const props = defineProps<{
+  option: Option;
+}>();
+
+const inputValue = defineModel();
+</script>
+
+<template>
+  <label :for="props.option.label" class="flex items-center">
+    <input
+      type="checkbox"
+      name=""
+      :id="props.option.value"
+      :value="props.option.value"
+      class="base-checkbox"
+      v-model="inputValue"
+    />
+    <div class="relative mr-1">
+      <img src="@/assets/images/checkbox-default.svg" class="w-5 h-5" />
+      <img src="@/assets/images/checkbox-checked.svg" class="w-5 h-5 absolute inset-0 opacity-0" />
+    </div>
+    <span>{{ props.option.label }}</span>
+  </label>
+</template>
+
+<style lang="postcss">
+.base-checkbox {
+  @apply w-0 h-0 opacity-0 absolute;
+
+  &:checked {
+    & + span {
+      @apply text-primary-500;
+    }
+
+    & ~ div {
+      & > img {
+        &:first-child {
+          @apply opacity-0;
+          @apply transition-opacity;
+        }
+
+        &:last-child {
+          @apply opacity-100;
+          @apply transition-opacity;
+        }
+      }
+    }
+  }
+}
+</style>
