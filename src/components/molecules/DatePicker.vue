397680c38e3518abbad5816531f924c818516cfe
AndyWu <s0939859391@gmail.com>
2024-06-12 22:29:30 +0800

    feat: add DatePicker

---
+++ src/components/molecules/DatePicker.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
+import BaseInput from '@/components/atoms/BaseInput.vue';
+
+const date = defineModel();
+const masks = ref({
+  input: 'YYYY-MM-DD'
+});
+</script>
+
+<template>
+  <DatePicker v-model="date" :masks="masks">
+    <template #default="{ togglePopover, inputValue }">
+      <BaseInput v-bind:model-value="inputValue" readonly @click="togglePopover" />
+    </template>
+  </DatePicker>
+</template>
