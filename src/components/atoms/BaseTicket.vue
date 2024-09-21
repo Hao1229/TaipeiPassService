127e122bbb83f83f31147680c63ce4e0fba47c85
AndyWu <s0939859391@gmail.com>
2024-09-21 14:15:37 +0800

    feat: update ticket style

--- src/components/atoms/BaseTicket.vue
+++ src/components/atoms/BaseTicket.vue
@@ -24,13 +24,32 @@ const dateText = computed(() => {
 </script>
 
 <template>
-  <div class="grid grid-cols-4 border border-grey-200 rounded-lg p-3 min-h-24">
-    <div class="flex justify-center items-center">
-      <img :src="props.img" :alt="props.name" />
+  <div class="base-ticket">
+    <div class="base-ticket__main-content">
+      <div class="flex justify-center items-center col-span-2">
+        <img :src="props.img" :alt="props.name" class="rounded-md" />
+      </div>
+      <div class="flex flex-col justify-between col-span-3 px-3">
+        <span class="font-semibold text-grey-800">{{ props.name }}</span>
+        <span class="text-sm text-grey-400">{{ props.date }} {{ dateText }}</span>
+      </div>
     </div>
-    <div class="flex flex-col justify-between col-span-3 px-3">
-      <span class="font-semibold text-grey-600">{{ props.name }}</span>
-      <span class="text-sm text-grey-400">{{ props.date }} {{ dateText }}</span>
+    <div class="rounded-[10px] flex items-center bg-white">
+      <img src="@/assets/images/dash.svg" />
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.base-ticket {
+  @apply grid grid-cols-5;
+  @apply bg-grey-100 rounded-[10px];
+  @apply min-h-28;
+
+  &__main-content {
+    @apply col-span-4;
+    @apply grid grid-cols-5;
+    @apply bg-white rounded-[10px] py-4 px-2;
+  }
+}
+</style>
