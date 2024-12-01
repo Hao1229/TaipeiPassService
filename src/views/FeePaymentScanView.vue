41a71e9172f35ee832e3ad1584f87e0ca859d4a5
Tanvitsao <tanvitsao@easontech.com.tw>
2024-12-01 22:00:30 +0800

    fix: fee-payment scan data

--- src/views/FeePaymentScanView.vue
+++ src/views/FeePaymentScanView.vue
@@ -6,10 +6,10 @@ import router from '@/router';
 
 const handleScan = (event: { data: string | null }) => {
   if (event.data) {
-    const result = JSON.parse(event.data);
-    console.log('result:', result);
+    const result: { name: string; data: { id: string } } = JSON.parse(event.data);
+    console.log('result:', result.data);
 
-    router.push({ name: 'fee-payment-result', params: { id: result.id } });
+    router.push({ name: 'fee-payment-result', params: { id: result.data.id } });
   }
 };
 
@@ -19,7 +19,12 @@ useHandleConnectionData(handleScan);
 
 onMounted(() => {
   // setTimeout(() => {
-  //   const scanString: string = JSON.stringify({ id: 'fee-1' });
+  //   const json = {
+  //     name: 'qr_code_scan',
+  //     data: { id: 'fee-1' }
+  //   };
+  //   const scanString: string = JSON.stringify(json);
+  //   console.log('scanString:', scanString);
   //   handleScan({ data: scanString });
   // }, 2000);
 });
