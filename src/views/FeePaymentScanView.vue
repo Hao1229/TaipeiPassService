2876835e5f3dda7e24b208817715fa0dfc38b86f
Tanvitsao <tanvitsao@easontech.com.tw>
2024-12-01 22:33:13 +0800

    feat: fee-payment add error msg handle

--- src/views/FeePaymentScanView.vue
+++ src/views/FeePaymentScanView.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useConnectionMessage } from '@/composables/useConnectionMessage';
 import { useHandleConnectionData } from '@/composables/useHandleConnectionData';
 import router from '@/router';
+import BaseDialog from '@/components/atoms/BaseDialog.vue';
 
 const handleScan = (event: { data: string | null }) => {
   if (event.data) {
     const result: { name: string; data: { id: string } } = JSON.parse(event.data);
     console.log('result:', result.data);
 
-    router.push({ name: 'fee-payment-result', params: { id: result.data.id } });
+    if (result.data.id) {
+      router.push({ name: 'fee-payment-result', params: { id: result.data.id } });
+    } else {
+      isErrorDialogOpen.value = true;
+    }
   }
 };
 
 useConnectionMessage('qr_code_scan', null);
 
 useHandleConnectionData(handleScan);
+
+const isErrorDialogOpen = ref(false);
 
 onMounted(() => {
   // setTimeout(() => {
@@ -29,3 +36,14 @@ onMounted(() => {
   // }, 2000);
 });
 </script>
+
+<template>
+  <BaseDialog
+    v-model="isErrorDialogOpen"
+    :isAlert="true"
+    title="讀取失敗"
+    content="此類型的行動條碼不適用"
+    positiveText="確認"
+    @onPositiveClick="isErrorDialogOpen = false"
+  />
+</template>
